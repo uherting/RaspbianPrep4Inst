fac1d9e7e01a75ef7c0916fa038ea924e8d5d99b
uherting <uherting@gmx.de>
2019-08-20 20:54:55 +0200

    debugged write_raspbian.sh, modified dd2image4GB.sh

--- dd2image4GB.sh
+++ dd2image4GB.sh
@@ -11,18 +11,23 @@ if [ ${BNAME} = "dd2image4GB" ];then
   ADD_PARM="count=4000"
 fi
 
+TGT_DIR="/home/raspi_images/${1}"
 TS=`date +%Y%m%d_%H%M%S`
+of="${TGT_DIR}/${TS}__${2}.img"
 
 echo "doing the unmount of the mmc partitions"
 umount /dev/mmcblk0p1
 umount /dev/mmcblk0p2
 
-of="${1}_${TS}__${2}.img"
+if [ ! -d ${TGT_DIR} ]; then
+ mkdir ${TGT_DIR}
+fi
+
 echo "doing dd"
 time dd bs=4M ${ADD_PARM} \
         if=/dev/mmcblk0 \
         of=${of} \
         status=progress
 
-echo "doing bzip2"
-time bzip2 ${of}
+#echo "doing bzip2"
+#time bzip2 ${of}
--- write_raspbian.sh
+++ write_raspbian.sh
@@ -3,8 +3,7 @@
 # This scripts writes the latest image file to the SD card.
 # The latest image is determined by the date/time stamp given in the file name.
 
-if [ "`whoami`" != "root" ]
-then
+if [ "`whoami`" != "root" ]; then
   echo "NOTE: For execution of this script you need root use priviledges. Script stops here."
   exit 99
 fi
@@ -14,34 +13,27 @@ DNAME=`dirname $0`
 
 . ${DNAME}/mod.conf
 
-if [ $# -gt 2 ]
-then
+if [ $# -gt 2 ]; then
   echo "Usage: ${BNAME}.sh <write_image_yn> <image_file> <target_device>"
   echo "       Parameter <write_image_yn>: either 'y' or 'n'. Allows / permits writing to SD card"
   echo "       More than three parameters are not allowed."
-  echo "       Both parameters are optional."
+  echo "       Parameters #2 and #3 are optional."
   echo "       If you want to supply the <target_device> then you have to supply the <image_file>, too."
   exit 1
 fi
 
 WRITE_IMG=0
-if [ "$1" == "y" ]; then
+if [ "${1}" == "y" ]; then
   WRITE_IMG=1
-if
-
-if [ $# -eq 3 ]; then
-  DEVICE_WR=$3
 fi
-
 
 # check if given file exist 
 # if no file name was given try to take the latest
 # according to the timestamp in file in the directory ${IMG_LOCATION_EDIT}
-if [ $# -eq 2 ]; then
+if [ $# -gt 1 ]; then
   IMG_FILE=$2
 
-  if [ ! -f ${IMG_FILE} ]
-  then
+  if [ ! -f ${IMG_FILE} ]; then
     echo "Image file was not found: ${IMG_FILE}"
     exit 2 
   fi
@@ -49,13 +41,17 @@ else
   echo "Checking for newest image file according to the timestamp in file"
   IMG_FILE=`ls ${IMG_LOCATION_EDIT}/*.img | sort | tail -n 1`
 
-  if [ ! -f ${IMG_FILE} ]
-  then
+  if [ ! -f ${IMG_FILE} ]; then
     echo "No image file found in directory ${IMG_LOCATION_EDIT}"
     exit 3 
   fi
 fi
 
+if [ $# -eq 3 ]; then
+  DEVICE_WR=$3
+fi
+
+# check whether the target device exists
 if [ -e ${DEVICE_WR} ]; then
   # unmount recently inserted card if necessary
   if [ -e ${DEVICE_WR}p1 ]; then
@@ -65,7 +61,8 @@ if [ -e ${DEVICE_WR} ]; then
     umount ${DEVICE_WR}p2
   fi
 
-  if [ ${WRITE_IMG} -eq 0 } ]; then
+  # write at all?
+  if [ ${WRITE_IMG} -eq 0 ]; then
     echo "No writing to the SD card takes place as requested by parameter #1."
   else
     echo "${IMG_FILE} gets written to SD card at ${DEVICE_WR}"
@@ -82,8 +79,11 @@ if [ -e ${DEVICE_WR} ]; then
     time dd bs=4M if=${IMG_FILE} of=${DEVICE_WR} status=progress
     sync
   fi
-else
+fi
+
+if [ ! -e ${DEVICE_WR} ]; then
   echo "ERROR: the device ${DEVICE_WR} does not exist."
 fi
+
 echo "finished at `date`"
 
